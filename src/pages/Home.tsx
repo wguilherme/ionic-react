00592d24937e0c9897dae99cd61f48e578489e6b
W. Guilherme <ravok123@gmail.com>
2020-12-05 23:42:57 -0300

    add: ion list and ion item

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonList, IonPage, IonTitle, IonToolbar, IonItem, IonText } from '@ionic/react';
 import React from 'react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
@@ -6,17 +6,23 @@ import './Home.css';
 const Home: React.FC = () => {
   return (
     <IonPage>
-      <IonHeader>
+      <IonHeader >
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle>Hello World</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
+            <IonTitle size="large">Hello World</IonTitle>
           </IonToolbar>
         </IonHeader>
+
+            <IonList>
+              <IonItem>
+                <IonText>Teste</IonText>
+              </IonItem>
+            </IonList>
         <ExploreContainer />
       </IonContent>
     </IonPage>
