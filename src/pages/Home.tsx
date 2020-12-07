afa51d36ca8f286c2671ed0be1c6333c4c2eaf93
W. Guilherme <ravok123@gmail.com>
2020-12-07 01:19:21 -0300

    add: simple loop item with key

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,6 +4,9 @@ import ExploreContainer from '../components/ExploreContainer';
 import Simple from '../components/simple';
 import './Home.css';
 
+// simple array for loop
+let simpleArray = ["item", 2, 3, "quatro", 5, 6, false]
+
 const Home: React.FC = () => {
   return (
     <IonPage>
@@ -19,10 +22,18 @@ const Home: React.FC = () => {
           </IonToolbar>
         </IonHeader>
 
+  
+
             <IonList>
-                {Array(...Array(10)).map((v, i) => <Simple indice={i} simpleProps="Simple Props" />)}
-             
+                {Array(...Array(10)).map((v, i) => <Simple  key={i} indice={i} simpleProps="Simple Props" />)}
             </IonList>
+
+
+        <h2>Simple Loops</h2>
+        {/* Simple array loop with map */}
+        <IonList>
+            {simpleArray.map((item, i) => <IonItem key={i} >{item}</IonItem>)}
+        </IonList>
 
 
 
