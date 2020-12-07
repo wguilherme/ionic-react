45ee87f304850ff1b01b7339f99febdca4772703
W. Guilherme <ravok123@gmail.com>
2020-12-07 01:07:04 -0300

    add: shortest way to repeat a component

---
+++ src/components/simple.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+interface SimpleProps { }
+
+const Simple: React.FC<SimpleProps> = () => {
+  return (
+   <p>Simple Component</p>
+  );
+};
+
+export default Simple;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,7 @@
 import { IonContent, IonHeader, IonList, IonPage, IonTitle, IonToolbar, IonItem, IonText } from '@ionic/react';
 import React from 'react';
 import ExploreContainer from '../components/ExploreContainer';
-import Repeat from '../components/Repeat';
+import Simple from '../components/simple';
 import './Home.css';
 
 const Home: React.FC = () => {
@@ -21,11 +21,14 @@ const Home: React.FC = () => {
 
             <IonList>
               <IonItem>
+                {Array(...Array(10)).map((v, i) => <Simple/>)}
                 <IonText>Teste</IonText>
               </IonItem>
             </IonList>
         <ExploreContainer />
-        <Repeat />
+
+
+
       </IonContent>
     </IonPage>
   );
